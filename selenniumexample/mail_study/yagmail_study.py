28077969def2dc50b13ce3141636e9c19d44f530
xiaochen <761338269@qq.com>
2022-06-20 02:09:47 +0800

    利用三方插件，yagmail发送邮件，可以批量发送，更加简单

---
+++ selenniumexample/mail_study/yagmail_study.py
@@ -0,0 +1,20 @@
+# !/usr/bin python3
+# encoding: utf-8 -*-
+# @file     :yagmail_study.py
+# @author   : 声培 
+# @Time     : 2022/6/20 0020 1:45
+
+import smtplib,yagmail
+#调用SMTP发件服务
+from email.mime.text import MIMEText    #导入纯文本的邮件模板类
+from email.mime.multipart import MIMEMultipart
+
+
+yagindex = yagmail.SMTP(user="761338269@qq.com",password='bznipopoeqngbchg',host='smtp.qq.com')
+Yag_contents = ['这是一个yagmail发送邮件正文的实例']
+yagindex.send(['2478703203@qq.com','761338269@qq.com'],'Yagmail主题实例',Yag_contents)
+
+# 发送带附件的邮件
+yagindex = yagmail.SMTP(user="761338269@qq.com",password='bznipopoeqngbchg',host='smtp.qq.com')
+Yag_contents = ['这是一个yagmail发送邮件正文的实例']
+yagindex.send(['2478703203@qq.com','761338269@qq.com'],'Yagmail主题实例',Yag_contents,[r"E:\pythonProject\selennium3_study\selenniumexample\mail_study\1.jpeg"])
