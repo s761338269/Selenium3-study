1d031b4c1806a82b2f5b35aa25bf08d1a8baea04
xiaochen <761338269@qq.com>
2022-06-20 23:48:07 +0800

    分离测试数据，用了excel方式。还有用例结束截图，helper是excel表格提取方法

---
+++ selenniumexample/PO_study/common/helper.py
@@ -0,0 +1,54 @@
+# !/usr/bin python3
+# encoding: utf-8 -*-
+# @file     :helper.py
+# @author   : 声培 
+# @Time     : 2022/6/20 0020 20:12
+import os.path
+
+import xlrd
+
+
+class Helper:
+    def readExcels(self,rowx):
+        """
+
+        :param rowx:是行数
+        :return: 返回的一共有多少行
+        """
+        book = xlrd.open_workbook(r'..\data\data.xls','r')
+        table = book.sheet_by_index(0)
+        return table.row_values(rowx)
+
+    def readusername(self,rowx):
+        """
+
+        :param rowx: 代表返回的是第几行
+        :return:    返回第rowx行的用户名
+        """
+        return str(self.readExcels(rowx)[0])
+
+
+    def readuserpsd(self,rowx):
+        """
+
+        :param rowx: 代表返回的是第几行
+        :return:    返回第rowx行的密码
+        """
+        return str(self.readExcels(rowx)[1])
+
+    def exceptText(self,rowx):
+        """
+
+        :param rowx: 代表返回的是第几行
+        :return:    返回第rowx行的预期结果
+        """
+        return str(self.readExcels(rowx)[2])
+
+    def dirname(self,filename,filepath='data'):
+        """
+        生成测试报告时，需要用例路径或者生成路径可能会用到
+        :param filename: 文件名
+        :param filepath: 文件路径
+        :return:
+        """
+        return os.path.join(os.path.dirname(os.path.dirname(__file__)),filepath,filename)
