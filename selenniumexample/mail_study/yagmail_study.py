55120b03650e2e7f8b3b2486e94b2c3c4d9c6168
xiaochen <761338269@qq.com>
2022-06-21 02:01:58 +0800

    删除隐私

--- selenniumexample/mail_study/yagmail_study.py
+++ selenniumexample/mail_study/yagmail_study.py
@@ -15,6 +15,6 @@ Yag_contents = ['这是一个yagmail发送邮件正文的实例']
 yagindex.send(['24******03@qq.com','761*****9@qq.com'],'Yagmail主题实例',Yag_contents)
 
 # 发送带附件的邮件
-yagindex = yagmail.SMTP(user="761338269@qq.com",password='bznipopoeqngbchg',host='smtp.qq.com')
+yagindex = yagmail.SMTP(user="761338269@qq.com",password='bz********bchg',host='smtp.qq.com')
 Yag_contents = ['这是一个yagmail发送邮件正文的实例']
 yagindex.send(['24******203@qq.com','76******69@qq.com'],'Yagmail主题实例',Yag_contents,[r"E:\pythonProject\selennium3_study\selenniumexample\mail_study\1.jpeg"])
